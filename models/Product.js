153d34c0cb0e0b5000c2b82983d32f9c5d8c1193
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2023-01-03 18:27:38 +0000

    updated product model

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,7 @@ const { DATE } = require("sequelize");
 const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
 const sequelize = require("../config/connection");
+const Category = require("./Category");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
