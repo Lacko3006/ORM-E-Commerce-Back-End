1f1e31361e20d7681bafb2b897e38f71fa814751
Sam Laxton <samlaxton@sams-air-2.home>
2023-01-05 18:58:19 +0000

    added DELETE routes to categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,8 +64,25 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryDelete = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryDelete) {
+      res.status(404).json({ message: "No category found with relevant id" });
+      return;
+    }
+
+    res
+      .status(200)
+      .json(`Category with ID: ${req.params.id} deleted from database.`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
