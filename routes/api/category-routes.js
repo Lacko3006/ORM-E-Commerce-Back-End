9e9659eb89c485ceab10b8435a355094902f4eac
Sam Laxton <samlaxton@sams-air-2.home>
2023-01-05 18:16:55 +0000

    added category PUT

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -22,10 +22,10 @@ router.get("/:id", async (req, res) => {
     const categoryGetId = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
-  if (!categoryGetId) {
-    res.status(404).json({ message: "No category found with relevant id"});
-    return;
-  }
+    if (!categoryGetId) {
+      res.status(404).json({ message: "No category found with relevant id" });
+      return;
+    }
     res.status(200).json(categoryGetId);
   } catch (err) {
     res.status(500).json(err);
@@ -36,15 +36,32 @@ router.post("/", async (req, res) => {
   // create a new category
   try {
     const newCategory = await Category.create({
-      category_name: req.body.category_name, });
-      res.status(200).json(newCategory); }
-      catch (err) {
-        res.status(400).json(err);
-      }
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    const categoryPut = await Category.findByPk(req.params.id);
+
+    if (!categoryPut) {
+      res.status(404).json({ message: "No category found with relevant id" });
+      return;
+    }
+    res.status(200).json(categoryPut);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
